72d99541791903a1930d4bfacdcb1eee3858e4ec
theapekshya <139621450+theapekshya@users.noreply.github.com>
2023-10-23 22:42:48 +0545

    Js_Operator

---
+++ second.js
@@ -0,0 +1,26 @@
+// Js  Operator 
+
+let x;
+x = 10;        //assignment operator
+document.write(x,"<br>")
+
+let a,b,c,d;
+a = x + 3;      // arithmetic operator
+document.write(a,"<br>")
+b = x / 3;      // division operator
+document.write(b,"<br>")
+c = x * 3;      // multiply operator
+document.write(c,"<br>")
+d = x % 3;      // remainder
+document.write(d,"<br>")
+
+// power
+let m ;
+m = x ** 3;
+document.write(m,"<br>")
+
+//concatinate
+let y = 5;
+let z = "5";
+console.log(y+z)
+
